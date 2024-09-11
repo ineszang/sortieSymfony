4048a40506b6183d6a5f379dac7aff5121ed92bb
Inès Z. <ines.zang2023@campus-eni.fr>
2024-09-11 16:56:53 +0200

    reset securitycontroller

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -14,38 +14,24 @@ use Doctrine\ORM\EntityManagerInterface;
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(Request $request, AuthenticationUtils $authenticationUtils,
+    public function login(AuthenticationUtils $authenticationUtils,
          EntityManagerInterface $entityManager): Response
     {
-        // Récupération des paramètres 
-        $form = $request->get('form');
-        /*$paramUsername = $request->request->get('pseudo');
-        $paramPassword = $request->request->get('mot_de_passe');*/
-
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
 
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-       /* $repository = $entityManager->getRepository(Participant::class);
-        $userDB = $repository->findByUser($paramUsername, $paramPassword);*/
-
-       // var_dump($paramUsername);
-       var_dump($form);
-
         /*if ( $userDB) {
             return $this->redirectToRoute('app_home', [
                 'user' => $userDB
             ]);
         }*/
-
-        
+    
         return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
-            //'pseudo' => $paramUsername
-           // 'mot_de_passe' => $paramPassword
         ]);
     }
 
