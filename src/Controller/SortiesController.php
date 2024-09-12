e1a1cecd98614e1ef6527d27f8dd5109c1f0f4a6
melaine <melaine.favennec@gmail.com>
2024-09-12 09:28:57 +0200

    ajout modif controller

--- src/Controller/SortiesController.php
+++ src/Controller/SortiesController.php
@@ -7,6 +7,8 @@ use App\Entity\Participant;
 use App\Entity\Site;
 use App\Entity\Sortie;
 use App\Form\CreationSortieType;
+use App\Repository\SiteRepository;
+use App\Repository\SortieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
@@ -79,6 +81,68 @@ class SortiesController extends AbstractController
             "sortieForm" => $sortieForm,
             'user' => $violette,
             'error' => $error
+        ]);
+    }
+
+    #[Route(['/allSorties'], name: 'app_allSorties')]
+    public function indexSorties(SortieRepository $sortieRepository, SiteRepository $siteRepository, Request $request): Response
+    {
+        //TODO : trouver le user en ligne
+        $utilisateur = "Melaine F.";
+
+        $site = $request->query->get('site');
+        $recherche = $request->query->get('recherche');
+        $dateStart = $request->query->get('dateStart');
+        $dateEnd = $request->query->get('dateEnd');
+        $mesSorties = $request->query->get('mesSorties');
+        $mesInscriptions = $request->query->get('mesInscriptions');
+        $pasMesInscriptions = $request->query->get('pasMesInscriptions');
+        $sortiesFinies = $request->query->get('sortiesFinies');
+
+        $sorties = $sortieRepository->findPublishedSorties();
+        $sites = $siteRepository->findAll();
+
+
+
+        //
+        //chercher dans la bdd
+        //$sites = $siteRepository->findBySearchParameters();
+
+
+
+
+        return $this->render('sorties/allSorties.html.twig', [
+            'utilisateur' => $utilisateur,
+            'sorties' => $sorties,
+            'sites' => $sites,
+            'site' => $site,
+            'recherche' => $recherche,
+            'dateStart' => $dateStart,
+            'dateEnd' => $dateEnd,
+            'mesSorties' => $mesSorties,
+            'mesInscriptions' => $mesInscriptions,
+            'pasMesInscriptions' => $pasMesInscriptions,
+            'sortiesFinies' => $sortiesFinies,
+        ]);
+    }
+
+
+    #[Route(['/detailsSorties/{id}'], name: 'app_details_sorties')]
+    public function indexDetailsSortie(int $id,SortieRepository $sortieRepository): Response
+    {
+        //TODO : trouver le user en ligne
+        $utilisateur = "Melaine F.";
+
+        $sortie = $sortieRepository->findOneBySomeField($id);
+
+
+
+
+
+        return $this->render('sorties/detailsSortie.html.twig', [
+            'utilisateur' => $utilisateur,
+            'sortie' => $sortie,
+
         ]);
     }
 }
