c64697d61173810ff72ac7dc5a7437116da66a6a
melaine <melaine.favennec@gmail.com>
2024-09-17 11:09:34 +0200

    bloquer user si pas actif

--- src/Security/LoginAuthenticator.php
+++ src/Security/LoginAuthenticator.php
@@ -2,11 +2,13 @@
 
 namespace App\Security;
 
+use App\Repository\ParticipantRepository;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
@@ -19,11 +21,13 @@ use Symfony\Component\Security\Http\Util\TargetPathTrait;
 class LoginAuthenticator extends AbstractLoginFormAuthenticator
 {
     use TargetPathTrait;
+    private ParticipantRepository $participantRepository;
 
     public const LOGIN_ROUTE = 'app_login';
 
-    public function __construct(private UrlGeneratorInterface $urlGenerator)
+    public function __construct(private UrlGeneratorInterface $urlGenerator, ParticipantRepository $participantRepository)
     {
+        $this->participantRepository = $participantRepository;
     }
 
     public function authenticate(Request $request): Passport
@@ -33,6 +37,13 @@ class LoginAuthenticator extends AbstractLoginFormAuthenticator
         $csrfToken = $request->request->get('_csrf_token');
 
         $request->getSession()->set(SecurityRequestAttributes::LAST_USERNAME, $username);
+
+        // Vérification personnalisée : par exemple, un service pour vérifier si l'utilisateur est actif
+        $participant = $this->participantRepository->findOneByPseudo($username);
+        if (!$participant || !$participant->isActif()) {
+            // Si l'utilisateur n'existe pas ou n'est pas actif
+            throw new CustomUserMessageAuthenticationException('Votre compte est désactivé.');
+        }
 
         return new Passport(
             new UserBadge($username),
