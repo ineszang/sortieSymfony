a6e1ff09effc0fc5401ffe36a400fe2874f05ebd
melaine <melaine.favennec@gmail.com>
2024-09-17 10:01:26 +0200

    amélioration et modification controller profile

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -20,65 +20,52 @@ class ProfilController extends AbstractController
 {
     #[Route('/profil', name: 'app_profil')]
     public function index(Request $request,ParticipantRepository $participantRepository, Security $security,
-        EntityManagerInterface $entityManager, FileUploaderService $fileUploaderService, SiteRepository $siteRepository, UserPasswordHasherInterface $userPasswordHasher): Response
+        EntityManagerInterface $entityManager, FileUploaderService $fileUploaderService, SiteRepository $siteRepository,
+                          UserPasswordHasherInterface $userPasswordHasher): Response
     {
-        //Changement de la liste des sites
+        //Chargement de la liste des sites
         $sites = $entityManager->getRepository(Site::class)->findAll();
 
         //Récupération des informations de l'utilisateur connecté
         $this->security = $security;
         $user = $this->security->getUser();
+        //Récupérer le participant connecté
         $participant = $participantRepository->findOneByPseudo($user->getUserIdentifier());
         $motDePasse = $participant->getMotDePasse();
         $photoProfile = $participant->getUrlPhoto();
 
-        $defaultSite = $participant->getSite()->getId();
+
         //Création du formulaire
         $form = $this->createForm(ProfilFormType::class, $user);
         $form->handleRequest($request);
 
 
-
-
         //Enregistrement des modifications du profil
-        if ($form->isSubmitted() && $form->get('mot_de_passe')->getData() != null) {
+        if ($form->isSubmitted()) {
             $imageFile = $form->get('image')->getData();
             if ($imageFile) {
                 $newFilename = $fileUploaderService->upload($imageFile);
-
                 $participant->setUrlPhoto('uploads/images/' . $newFilename);
             }
-            /** @var string $plainPassword */
-            $plainPassword = $form->get('mot_de_passe')->getData();
+            //On récupère le mot de passe si des champs sont renseignés
+            if($form->get('mot_de_passe')->getData() != null) {
+                $plainPassword = $form->get('mot_de_passe')->getData();
+                $hashedPassword = $userPasswordHasher->hashPassword($user, $plainPassword);
+                $participant->setMotDePasse($hashedPassword);
+            } else {
+                $participant->setMotDePasse($motDePasse);
+            }
+
+            //on renseigne le nouveau site
             $siteId = $request->get('site_id');
             $site = $siteRepository->find($siteId);
-
-            //Hachage du nouveau mot de passe
-            $hashedPassword = $userPasswordHasher->hashPassword($user, $plainPassword);
-            $participant->setMotDePasse($hashedPassword);
             $participant->setSite($site);
 
             $entityManager->persist($participant);
             $entityManager->flush();
 
-
-        } elseif ($form->isSubmitted()  && $form->get('mot_de_passe')->getData() == null) {
-            $imageFile = $form->get('image')->getData();
-            if ($imageFile) {
-                $newFilename = $fileUploaderService->upload($imageFile);
-
-                $participant->setUrlPhoto('uploads/images/' . $newFilename);
-            }
-            $siteId = $request->get('site_id');
-            $site = $siteRepository->find($siteId);
-            $participant->setSite($site);
-
-            $participant->setMotDePasse($motDePasse);
-            $entityManager->persist($participant);
-            $entityManager->flush();
-
+            return $this->redirectToRoute('app_profil');
         }
-
 
         return $this->render('profil/index.html.twig', [
             'ProfilForm' => $form->createView(),
