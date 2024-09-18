3c5f39926c5b4c7b86eb9af9521b3e86f058f3e0
Inès Z. <ines.zang2023@campus-eni.fr>
2024-09-18 16:53:19 +0200

    bug contraintes mdp reset

--- src/Form/ChangePasswordFormType.php
+++ src/Form/ChangePasswordFormType.php
@@ -46,7 +46,7 @@ class ChangePasswordFormType extends AbstractType
                 'invalid_message' => 'Les mots de passe ne correspondent pas.',
                 // Instead of being set onto the object directly,
                 // this is read and encoded in the controller
-                'mapped' => false,
+                
             ])
         ;
     }
